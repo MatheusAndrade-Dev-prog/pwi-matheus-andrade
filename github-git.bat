6c1240ad30006ed1412d6e58d038c2603e77e006
matheus Andrade <matheus.dev5424@gmail.com>
2024-08-30 21:45:26 -0300

    atualização

--- github-git.bat
+++ github-git.bat
@@ -1,10 +1,11 @@
 echo ON
 
 echo --- Configurando Usuario GitHUB ---
-
+echo
+echo
 git config --global user.name "matheus Andrade"
-git config --global user.email "matheus.dev5424@gmail.com"
-
+git config --global user.email matheus.dev5424@gmail.com
+git config list
 echo --- fim ---
 echo --- joao Lindo Saudades Por favor volta pra nois ---
 
