d1fcc79da7a6fd19708f27ccf5c5551eb598bf46
matheus Andrade <matheus.dev5424@gmail.com>
2024-09-15 15:47:49 -0300

    projeto do restautante

--- github-git.bat
+++ github-git.bat
@@ -1,11 +1,10 @@
 echo ON
 
 echo --- Configurando Usuario GitHUB ---
-echo
-echo
+
 git config --global user.name "matheus Andrade"
-git config --global user.email matheus.dev5424@gmail.com
-git config list
+git config --global user.email "matheus.dev5424@gmail.com"
+
 echo --- fim ---
 echo --- joao Lindo Saudades Por favor volta pra nois ---
 
